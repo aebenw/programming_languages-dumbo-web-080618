bfdfc9ac9168a5e4dce1fbbccd8ecd0020b209f4
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-23 18:22:31 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,7 +10,7 @@ def reformat_languages(languages)
     types << style
     hash.each do |lang, mohash|
       mohash.each do |type, specific|
-        new_hash[lang] = {[:type] = specific, [:style] = types}
+        new_hash[lang] = {[:type] = specific, [:style] = [types]}
       end
     end
   end
